597388bd8bd733ebb8090f7b67c66c2aaa7f1770
Mohammad Fathi <40602800+mimfa@users.noreply.github.com>
2023-08-29 22:17:56 +0330

    Add some other abilities

--- view/page/user/groups.php
+++ view/page/user/groups.php
@@ -4,10 +4,20 @@ use MiMFa\Module\Table;
 use MiMFa\Library\DataBase;
 echo "<div class='page'>";
 MODULE("Table");
-$mod = new Table(DataBase::DoSelect(\_::$CONFIG->DataBasePrefix."UserGroup"));
+$mod = new Table(\_::$CONFIG->DataBasePrefix."UserGroup");
 $mod->RowLabelsKeys = ["ID"];
 $mod->ExcludeColumnKeys = ["CreateTime", "UpdateTime", "MetaData"];
-$mod->Changeable = true;
+$std = new stdClass();
+$std->Type="range";
+$std->Attributes=["min"=>0,"max"=>9];
+$mod->Updatable = true;$mod->CellTypes = [
+    "ID"=>"number",
+    "Access"=>$std,
+    "Status"=>[-1=>"Blocked",0=>"Undifined",1=>"Activated"],
+    "MetaData"=>"json",
+    "CreateTime"=>"datetime",
+    "UpdateTime"=>"datetime"
+    ];
 $mod->Draw();
 echo "</div>";
 ?>
--- view/page/user/users.php
+++ view/page/user/users.php
@@ -4,10 +4,24 @@ use MiMFa\Module\Table;
 use MiMFa\Library\DataBase;
 echo "<div class='page'>";
 MODULE("Table");
-$mod = new Table(DataBase::DoSelect(\_::$CONFIG->DataBasePrefix."User"));
+$mod = new Table(\_::$CONFIG->DataBasePrefix."User");
 $mod->RowLabelsKeys = ["ID", "Signature", "Email"];
 $mod->ExcludeColumnKeys = ["CreateTime", "UpdateTime", "MetaData"];
-$mod->Changeable = true;
+$mod->Updatable = true;
+$mod->CellTypes = [
+    "ID"=>"number",
+    "GroupID"=>DataBase::DoSelectPairs(\_::$CONFIG->DataBasePrefix."UserGroup","`ID`", "`Title`"),
+    "Gender"=>["Male"=>"Male","Female"=>"Female","X"=>"X (Transexual)"],
+    "Status"=>[-1=>"Blocked",0=>"Deactivated",1=>"Activated"],
+    "Image"=>"image",
+    "Bio"=>"strings",
+    "Contact"=>"tel",
+    "Email"=>"email",
+    "Password"=>"password",
+    "MetaData"=>"json",
+    "CreateTime"=>"datetime",
+    "UpdateTime"=>"datetime"
+    ];
 $mod->Draw();
 echo "</div>";
 ?>
