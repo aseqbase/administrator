240f4f65db1e44f8151854718ee6fd0b46311e95
Mohammad Fathi <40602800+mimfa@users.noreply.github.com>
2023-11-23 13:31:48 +0330

    Update

--- view/part/bar-menu.php
+++ view/part/bar-menu.php
@@ -1,11 +1,11 @@
 <?php MODULE("BarMenu");
 $module = new \MiMFa\Module\BarMenu();
 $module->Items = array(
-		array("Name"=>"Menu","Link"=>"","Image"=>"/file/symbol/menu.png", "Attributes"=>"onclick='viewSideMenu()'"),
-		array("Name"=>"POSTS", "Access"=>\_::$CONFIG->AdminAccess,"Link"=>"/content/posts","Image"=>"/file/symbol/document.png"),
-		array("Name"=>"HOME", "Access"=>\_::$CONFIG->AdminAccess,"Link"=>"/home","Image"=>"/file/symbol/home.png"),
-		array("Name"=>"USERS", "Access"=>\_::$CONFIG->AdminAccess,"Link"=>"/user/users","Image"=>"/file/symbol/user.png"),
-		array("Name"=>"SYSTEMS", "Access"=>\_::$CONFIG->AdminAccess,"Link"=>"/system/information","Image"=>"/file/symbol/operation.png"),
+		array("Name"=>"Menu","Link"=>"","Image"=>getFullUrl("/file/symbol/menu.png"), "Attributes"=>"onclick='viewSideMenu()'"),
+		array("Name"=>"POSTS", "Access"=>\_::$CONFIG->AdminAccess,"Link"=>"/content/posts","Image"=>getFullUrl("/file/symbol/document.png")),
+		array("Name"=>"HOME", "Access"=>\_::$CONFIG->AdminAccess,"Link"=>"/home","Image"=>getFullUrl("/file/symbol/home.png")),
+		array("Name"=>"USERS", "Access"=>\_::$CONFIG->AdminAccess,"Link"=>"/user/users","Image"=>getFullUrl("/file/symbol/user.png")),
+		array("Name"=>"SYSTEMS", "Access"=>\_::$CONFIG->AdminAccess,"Link"=>"/system/information","Image"=>getFullUrl("/file/symbol/operation.png")),
 		);
 $module->Draw();
 ?>
