f86e0e320d479a804410f8f16a090b5ababc4ff0
Mohammad Fathi <40602800+mimfa@users.noreply.github.com>
2024-03-13 22:46:37 +0330

    Update configuration.php

--- view/part/system/configuration.php
+++ view/part/system/configuration.php
@@ -1,10 +1,14 @@
 <?php
 ACCESS(\_::$CONFIG->SuperAccess);
-//if(RECEIVE(null,"POST")) echo \MiMFa\Library\Revise::HandleForm(\_::$CONFIG);
-//else {
-//    $form = \MiMFa\Library\Revise::GetForm(\_::$CONFIG);
-//    $form->Title = "Edit Configuration";
-//    $form->Id = "EditConfiguration";
-//    $form->Draw();
-//}
+if(RECEIVE("restore","GET")) echo \MiMFa\Library\Revise::Restore(\_::$TEMPLATE)
+    ?\MiMFa\Library\HTML::Success("Data Restored Successfully!")
+    :\MiMFa\Library\HTML::Warning("Data is restored!");
+if(RECEIVE(null,"POST")) echo \MiMFa\Library\Revise::HandleForm(\_::$CONFIG);
+else {
+    $form = \MiMFa\Library\Revise::GetForm(\_::$CONFIG);
+    $form->Title = "Edit Configuration";
+    $form->Id = "EditConfiguration";
+    $form->Buttons = \MiMFa\Library\HTML::Button("Restore",\_::$PATH."?restore=true");
+    $form->Draw();
+}
 ?>
