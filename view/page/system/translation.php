d34f55d568b7af1358b83bc74d3ca1aafcda6d17
Mohammad Fathi <mimfapost@gmail.com>
2024-07-17 18:48:11 +0330

    Update translation.php

--- view/page/system/translation.php
+++ view/page/system/translation.php
@@ -1,36 +1,32 @@
 <?php
 ACCESS(\_::$CONFIG->AdminAccess);
 use \MiMFa\Library\HTML;
+use \MiMFa\Library\Convert;
 if(isset($_GET["export"])){
-    $content = [""];
+    $cells = [""];
     $dic = [];
-    foreach (\MiMFa\Library\Translate::GetAll() as $value){
+    foreach (\MiMFa\Library\Translate::GetAll("ORDER BY `KeyCode` ASC") as $value){
         foreach ($value as $k=>$v)
-            $dic[$k] = preg_find("/[\s\W]/", $v)?('"'.str_replace("\"","\\\"",$v).'"'):$v;
-        $content[] = join(",",loop($dic, function($k, $v){ return $v;}));
+            $dic[$k] = $v;
+        $cells[] = loop($dic, function($k, $v){ return $v;});
         foreach ($dic as $k=>$v)
             $dic[$k] = null;
     }
-    $content[0] = join(",",loop($dic, function($k){return $k;}));
-    \MiMFa\Library\Local::Download(join("\r\n", $content), "Lexicon.csv");
+    $cells[0] = loop($dic, function($k){return $k;});
+    \MiMFa\Library\Local::Download(Convert::FromCells($cells), "Lexicon.csv");
     die;
 } elseif(isset($_GET["import"])){
     $c = 0;
     $keys = [];
-    $dic = [];
-    //$pat = '/(?<=^|\,)((([\"\'])[^\"]*\3)|([^\"\,][^\,]*)|(\s?))(?=$|\,)/m';
-    $pat = '/\,/m';
-    $cdic = [];
-    $data = code(urldecode(first($_POST)), $cdic);
-    foreach (preg_split("/(\r\n)|(\n\r)|\n/", $data) as $row) {
+    foreach (Convert::ToCells(urldecode(first($_POST))) as $row) {
         if($c===0) {
-            $keys = preg_split($pat, $row);
+            $keys = $row;
             for ($i = 0; $i < $length; $i++)
-                $keys[$i] = decode($keys[$i], $cdic);
+                $keys[$i] = $keys[$i];
         } else {
             $col = [];
-            foreach (preg_split($pat, $row) as $i=>$value)
-                if(isset($keys[$i])) $col[$keys[$i]] = trim(decode($value, $cdic), "\"'");
+            foreach ($row as $i=>$value)
+                if(isset($keys[$i])) $col[$keys[$i]] = $value;
             $dic[] = $col;
         }
         $c++;
@@ -44,64 +40,65 @@ if(isset($_GET["export"])){
         FLIP(HTML::Success("All key values cleared successfuly from the lexicon!"));
     else die(HTML::Error("There occured a problem!"));
 }
-
-MODULE("PrePage");
-$module = new MiMFa\Module\PrePage();
-$module->Title = "Translation";
-$module->Draw();
-if(!RECEIVE(null,"post")){
-    echo HTML::Center(
-        (RECEIVE("update")?HTML::Button("View Lexicon","/".\_::$DIRECTION):HTML::Button("Edit Lexicon","/".\_::$DIRECTION."?update=true")).
-        HTML::Button("Export Lexicon","/".\_::$DIRECTION."?export&".\_::$CONFIG->ViewHandlerKey."=value").
-        HTML::Button("Import Lexicon","
-    var input = document.createElement('input');
-    input.setAttribute('type', 'file');
-    input.onchange = evt => {
-        const [file] = input.files;
-        if (file) {
-            //URL.createObjectURL(file);
-            const reader = new FileReader();
-            reader.addEventListener('load', (event) => {
-                postData(
-                    '/".\_::$DIRECTION."?import&".\_::$CONFIG->ViewHandlerKey."=value',
-                    'data='+encodeURIComponent(event.target.result),
-                    'content',
-                    function(data, selector){
-						$(selector + ' .result').remove();
-						if(isEmpty(data)) load();
-						else {
-							data = (data !=null && typeof(data) == 'object')?data.statusText:data??'".__("The form submitted successfully!")."';
-							if(isSet(data) && !isEmpty(data)) $(selector).prepend(data);
-                            load();
-						}
-					}
-                );
-            });
-            reader.readAsText(file);
-        }
-    }
-    $(input).trigger('click');
-    return false;
-").
-        HTML::Button("Clear Lexicon","
-        if(confirm('Are you sure to clear all lexicon records?'))
-            postData(
-                '/".\_::$DIRECTION."?truncate&".\_::$CONFIG->ViewHandlerKey."=value',
-                {
-                    selector:'content',
-                    successHandler: function(data, selector){
-						$(selector + ' .result').remove();
-						if(isEmpty(data)) load();
-						else {
-							data = (data !=null && typeof(data) == 'object')?data.statusText:data??'".__("The form submitted successfully!")."';
-							if(isSet(data) && !isEmpty(data)) $(selector).prepend(data);
-                            load();
-						}
-					}
+else{
+    MODULE("PrePage");
+    $module = new MiMFa\Module\PrePage();
+    $module->Title = "Translation";
+    $module->Draw();
+    if(!RECEIVE(null,"post")){
+        echo HTML::Center(
+            (RECEIVE("update")?HTML::Button("View Lexicon","/".\_::$DIRECTION):HTML::Button("Edit Lexicon","/".\_::$DIRECTION."?update=true")).
+            HTML::Button("Export Lexicon","/".\_::$DIRECTION."?export&".\_::$CONFIG->ViewHandlerKey."=value").
+            HTML::Button("Import Lexicon","
+                var input = document.createElement('input');
+                input.setAttribute('type', 'file');
+                input.onchange = evt => {
+                    const [file] = input.files;
+                    if (file) {
+                        //URL.createObjectURL(file);
+                        const reader = new FileReader();
+                        reader.addEventListener('load', (event) => {
+                            postData(
+                                '/".\_::$DIRECTION."?import&".\_::$CONFIG->ViewHandlerKey."=value',
+                                'data='+encodeURIComponent(event.target.result),
+                                'content',
+                                function(data, selector){
+						            $(selector + ' .result').remove();
+						            if(isEmpty(data)) load();
+						            else {
+							            data = (data !=null && typeof(data) == 'object')?data.statusText:data??'".__("The form submitted successfully!")."';
+							            if(isSet(data) && !isEmpty(data)) $(selector).prepend(data);
+                                        load();
+						            }
+					            }
+                            );
+                        });
+                        reader.readAsText(file);
+                    }
                 }
-            );
-    ", ["class"=>"Error"])
-    );
+                $(input).trigger('click');
+                return false;
+            ").
+        HTML::Button("Clear Lexicon","
+            if(confirm('Are you sure to clear all lexicon records?'))
+                postData(
+                    '/".\_::$DIRECTION."?truncate&".\_::$CONFIG->ViewHandlerKey."=value',
+                    {
+                        selector:'content',
+                        successHandler: function(data, selector){
+						    $(selector + ' .result').remove();
+						    if(isEmpty(data)) load();
+						    else {
+							    data = (data !=null && typeof(data) == 'object')?data.statusText:data??'".__("The form submitted successfully!")."';
+							    if(isSet(data) && !isEmpty(data)) $(selector).prepend(data);
+                                load();
+						    }
+					    }
+                    }
+                );
+        ", ["class"=>"Error"])
+        );
+    }
+    echo HTML::Page(PART("table/lexicon", print:false));
 }
-echo HTML::Page(PART("table/lexicon", print:false));
 ?>
